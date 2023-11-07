f1067d598bc2bde26c80460ab08871b86377affb
sousky <hojun3338@naver.com>
2023-11-07 23:08:22 +0900

    2023_11_07_file_name_change

---
+++ 8-1_구구단 출력 프로그램/2023202032_남호준_8_1.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("출력하고 싶은 단 수를 입력: ");
+    scanf("%d", &a);
+
+    char file_name[1000];
+    sprintf(file_name, "%d_dan.txt", a);
+
+    FILE* wfp = fopen(file_name, "w");
+    if (wfp == NULL) {
+        perror("파일 열기 실패");
+        return 1;
+    }
+
+    for (int i = 1; i <= 9; i++) {
+        fprintf(wfp, "%d X %d = %d", a, i, a * i);
+        if (i != 9) fprintf(wfp,"\n");
+    }
+
+    fclose(wfp);
+    return 0;
+}
