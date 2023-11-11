35e8802df970e70589bb01bc676d899efac5cdb7
sousky <hojun3338@naver.com>
2023-11-12 04:55:11 +0900

    2023_11_12-made_8-4,fix

--- 5-4_로마숫자/2023202032_남호준_5-4.c
+++ 5-4_로마숫자/2023202032_남호준_5-4.c
@@ -21,7 +21,7 @@ void main()
 		for (int i = 0; i < b; i++)
 		{
 			len += 1;
-			arr = (char*)realloc(arr, sizeof(char) * len + 1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'C';
 		}
 		arr[len] = '\0';
@@ -29,19 +29,19 @@ void main()
 	else if (b >= 5 && b <= 8)
 	{
 		++len;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'D';
 		for (int i = 0; i < b - 5; i++)
 		{
 			len += 1;
-			arr = (char*)realloc(arr, sizeof(char) * len+1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'C';
 		}
 	}
 	else if (b == 9)
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'C';
 		++len;
 		arr[len-1] = 'M';
@@ -49,23 +49,20 @@ void main()
 	else
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'C';
 		++len;
 		arr[len-1] = 'D';
 	}
 
-
-
 	b = (a % 100 - a % 10) / 10;
-
 
 	if (b <= 3)
 	{
 		for (int i = 0; i < b; i++)
 		{
 			len += 1;
-			arr = (char*)realloc(arr, sizeof(char) * len + 1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'X';
 		}
 		arr[len] = '\0';
@@ -73,19 +70,19 @@ void main()
 	else if (b >= 5 && b <= 8)
 	{
 		++len;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'L';
 		for (int i = 0; i < b - 5; i++)
 		{
 			len += 1;
-			arr = (char*)realloc(arr, sizeof(char) * len + 1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'X';
 		}
 	}
 	else if (b == 9)
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'X';
 		++len;
 		arr[len-1] = 'C';
@@ -93,22 +90,20 @@ void main()
 	else
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'X';
 		++len;
 		arr[len-1] = 'L';
 	}
 
-
 	b = a % 10;
-
 
 	if (b <= 3)
 	{
 		for (int i = 0; i < b; i++)
 		{
 			len += 1;
-			arr = (char*)realloc(arr, sizeof(char) * len + 1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'I';
 		}
 		arr[len] = '\0';
@@ -116,19 +111,19 @@ void main()
 	else if (b >= 5 && b <= 8)
 	{
 		++len;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'V';
 		for (int i = 0; i < b - 5; i++)
 		{
 			++len;
-			arr = (char*)realloc(arr, sizeof(char) * len + 1);
+			arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 			arr[len-1] = 'I';
 		}
 	}
 	else if (b == 9)
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'I';
 		++len;
 		arr[len-1] = 'X';
@@ -136,13 +131,11 @@ void main()
 	else
 	{
 		len += 1;
-		arr = (char*)realloc(arr, sizeof(char) * len + 1);
+		arr = (char*)realloc(arr, sizeof(char) * (len + 1));
 		arr[len-1] = 'I';
 		++len;
 		arr[len-1] = 'V';
 	}
-
 	printf("%d=%d+%d+%d=%s, %d", a, (a - a % 100), (a % 100 - a % 10), b, arr, len);
-
 	free(arr);
 }
---
+++ 8-4_사전식 정렬/8-4_사전식 정렬-fgetc,이중 포인터.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#pragma warning(disable : 6031)
+
+void make_dictionary(char** a, int cnt);//sorting func
+int my_strcmp(const char* a, const char* b);//strcmp func: case sensitive regardless
+
+void main()
+{
+	FILE* rfp = fopen("words.txt", "r");//file ptr define
+	int c;//input constructor
+	int len = 0, cnt = 0;//word length & words cnt
+	int overlapping = 0;//overlapping delimiter
+	char** A = NULL;//words list
+
+	A = (char**)malloc(sizeof(char*));//std_alloc
+	*A = (char*)malloc(sizeof(char));//std_alloc
+
+	////////////word input///////////////////////////
+	while (1)//word input loop
+	{
+		if (feof(rfp) != 0) { printf("파일의 끝에 도달했습니다.\n");	break; }//file end inspect
+		c = fgetc(rfp);
+		if (c == '\n' || c == ';' || c == '\t' || c == ',' || c == '|')
+		{
+			if (overlapping == 1)
+				continue;
+			A[cnt][len] = '\0';
+			++cnt;
+			A = (char**)realloc(A, sizeof(char*) * (cnt+1));
+			*(A + cnt) = (char*)malloc(sizeof(char) * (len + 1));
+			len = 0;
+			++overlapping;
+		}
+		else
+		{
+			*(A + cnt) = (char*)realloc(*(A + cnt), sizeof(char) * (len + 1));
+			A[cnt][len] = c;
+			len++;
+			overlapping = 0;
+		}
+	}
+	/////////word input//////////////////////////////
+
+	make_dictionary(A, cnt);//sorting func call
+	
+
+
+	////////////file save/////////////////////
+	FILE* wfp = fopen("dictionary.txt", "w");
+	for (int i = 0; i < cnt; i++)
+	{
+		printf("%s\n", *(A + i));
+		fprintf(wfp, "%s\n", *(A + i));
+	}
+	/////////////////////////////////////////
+
+
+	for (int i = 0; i < cnt; i++)
+		free(A[i]=NULL);
+	free(A);
+	fclose(rfp);
+	fclose(wfp);
+}
+
+void make_dictionary(char** a, int cnt)
+{
+	int c = 0, j;
+	char tmp[20] = { NULL };
+
+	for (int i = 0; i < cnt; i++)
+	{
+		for (int k = i + 1; k < cnt; k++)
+		{
+			j = i;
+			c = my_strcmp(a[i], a[k]);
+			if (c > 0)
+			{
+				j = k;
+			}
+			strcpy(tmp, *(a + i));
+			strcpy(*(a + i), *(a + j));
+			strcpy(*(a + j), tmp);
+		}
+	}
+}
+
+int my_strcmp(const char* a, const char* b)
+{
+	char p[20];
+	char q[20];
+	strcpy(p, a);
+	strcpy(q, b);
+
+	for (int h = 0; p[h] != '\0'; h++)
+	{
+		if (p[h] >= 'A' && p[h] <= 'Z')
+			p[h] += 32;
+	}
+	for (int j = 0; q[j] != '\0'; j++)
+	{
+		if (q[j] >= 'A' && q[j] <= 'Z')
+			q[j] += 32;
+	}
+
+	int v = strcmp(p, q);
+
+	return v;
+}
