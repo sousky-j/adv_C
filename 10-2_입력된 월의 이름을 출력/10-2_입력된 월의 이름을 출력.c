6f4acc503abe29316337bc8b0f125b64d871b3bf
sousky <hojun3338@naver.com>
2023-11-16 19:01:14 +0900

    2023_11_16-10th code

--- 10-2_입력된 월의 이름을 출력/10-2_입력된 월의 이름을 출력.c
+++ 10-2_입력된 월의 이름을 출력/10-2_입력된 월의 이름을 출력.c
@@ -6,7 +6,24 @@ void main()
 {
 	enum month { January = 1, February, March, April, May, June, July, August, September, October, November, December };
 
-	int a,d=0;
+	int a;
 	printf("월 입력: ");
 	scanf("%d", &a);
+	enum month mm = a;
+	switch (mm)
+	{
+	case 1:		printf("%d월은 January입니다.", mm);		break;
+	case 2:		printf("%d월은 February입니다.", mm);	break;
+	case 3:		printf("%d월은 March입니다.", mm);		break;
+	case 4:		printf("%d월은 April입니다.", mm);		break;
+	case 5:		printf("%d월은 May입니다.", mm);			break;
+	case 6:		printf("%d월은 June입니다.", mm);		break;
+	case 7:		printf("%d월은 July입니다.", mm);		break;
+	case 8:		printf("%d월은 August입니다.", mm);		break;
+	case 9:		printf("%d월은 September입니다.", mm);	break;
+	case 10:	printf("%d월은 October입니다.", mm);		break;
+	case 11:	printf("%d월은 November입니다.", mm);	break;
+	case 12:	printf("%d월은 December입니다.", mm);	break;
+	default: printf("잘못된 월 수를 입력하였습니다.");		break;
+	}
 }
---
+++ 10-3_도서관 도서 대여 프로그램/10-3_도서관 도서 대여 프로그램.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#pragma warning (disable: 6031)
+
+struct book
+{
+	int id;//도서 ID
+	int odd;//남은 권 수
+	char name[50];//도서 제목
+};
+struct book b[21];//도서관
+
+struct borrow
+{
+	int id;
+	char name[50];
+};
+
+void initial_booklist(struct book b[21]);
+void manage_booklist(struct borrow* p, struct book b[21], int id, int* bilin);
+
+void main()
+{
+	int id;//id 입력
+	int bil=0;//빌린 권수
+	int* bilin = &bil;
+	struct borrow* p;//대여 도서
+	p = (struct borrow*)malloc(sizeof(struct borrow) * bil);// realloc 위한 malloc
+	initial_booklist(b);//도서관 도서 초기화
+
+	while (1)
+	{
+		printf("========== Book list ==========\n");
+		for (int i = 0; i < 21; i++)
+			printf("%d : %s\n", b[i].id, b[i].name);
+		printf("===============================\n");
+		printf("Type the book id to borrow(0 to end) : ");
+		scanf("%d", &id);
+		if (id == 0)	break;
+		else if (id < 1 || id > 21) {	printf("보유하지 않은 도서입니다. 돌아갑니다.\n\n"); continue;	}
+		p = (struct borrow*)realloc(p, sizeof(struct borrow) * (bil + 1));
+		manage_booklist(p, b, id, bilin);
+
+		printf("=========== Borrowed Book List ===========\n");
+		for (int i = 0; i < bil; i++)
+			printf("%d : %s\n", p[i].id, p[i].name);
+		printf("===========================================\n");
+	}
+}
+
+void manage_booklist(struct borrow* p, struct book b[21], int id, int* bilin)
+{
+	id--;
+	if (b[id].odd == 0)
+		printf("The Book is out of stock.\n");
+	else
+	{
+		*bilin += 1;
+		p = (struct borrow*)realloc(p, sizeof(struct borrow) * (*bilin));
+		b[id].odd -= 1;
+		p[*bilin-1].id = id+1;
+		strcpy(p[*bilin-1].name, b[id].name);
+		printf("Borrow a Book.\n");
+	}
+}
+
+void initial_booklist(struct book b[21])
+{
+	FILE* rfp = fopen("book.txt", "r");
+	for (int i = 0; i < 21; i++)
+	{
+		fgets(b[i].name, 50, rfp);
+		b[i].name[strlen(b[i].name) - 1] = '\0';
+		b[i].id = i + 1;
+		b[i].odd = 3;
+	}
+	fclose(rfp);
+}
